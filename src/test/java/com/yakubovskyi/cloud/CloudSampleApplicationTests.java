23fa0ef21b528251676abb08d6a21fb2ee6eadcf
a.yakubovskyi <andrii.yakubovskyi@profitsoft.dev>
2026-01-15 20:45:35 +0200

    init commit

---
+++ src/test/java/com/yakubovskyi/cloud/CloudSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakubovskyi.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
